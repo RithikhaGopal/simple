cbf6fce5ee2707fcaf727f114232a4575a6e8853
RithikhaGopal <grithikha29@gmail.com>
2025-09-22 14:07:33 +0530

    Second Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hi, I am Rithikha,MERN Stack Developer.</h1>
+      <h2>Bannari Amman Institute of Technology</h2>
     </div>
   );
 }
