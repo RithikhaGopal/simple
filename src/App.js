12b0a25b2f487eabf263905c5b7c1ada7e11a5bb
RithikhaGopal <grithikha29@gmail.com>
2025-09-22 14:02:16 +0530

    First change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I am Rithikha</h1>
+      <h1>Hi, I am Rithikha,MERN Stack Developer.</h1>
     </div>
   );
 }
